dfa4d6f60f743019944f81b154124d6612ca7f91
Thiendz91999 <90498839+Thiendz91999@users.noreply.github.com>
2021-09-11 17:10:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ const CONFIG = {
     introTitle: 'Hồng Trí Thiện',
     introDesc: `Đẹp trai nhất quả đất
     !!!!!
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    hello
+   hello `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'B: là đẹp trai  <33',
-    btnNo: 'A: đéo :3',
+    title: 'Vui lòng chọn A hoặc B',
+    desc: 'Ai là người đẹp trai nhất thế giới ',
+    btnYes: 'B: Hồng Trí Thiện <33',
+    btnNo: 'A: là tôi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
